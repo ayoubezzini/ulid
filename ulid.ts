f8a2b83efcc8eccbdc91d32be36034af50d64181
YaaY <ayoub.ezzini@youcan.shop>
2024-03-03 22:26:09 +0100

    docs: add symbol documentation

--- ulid.ts
+++ ulid.ts
@@ -1,8 +1,18 @@
-const ENCODING = '0123456789ABCDEFGHJKMNPQRSTVWXYZ'; // Crockford's Base32
+/** Crockford's Base32 characters used for encoding ULID components. */
+const ENCODING = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
+/** The length of the encoding character set. */
 const ENCODING_LEN = ENCODING.length;
+/** The length of the timestamp part of a ULID. */
 const TIME_LEN = 10;
+/** The length of the random part of a ULID. */
 const RANDOM_LEN = 16;
 
+/**
+ * Encodes a random sequence of a specified length into a base32 string.
+ * 
+ * @param length The length of the random sequence to generate.
+ * @return A base32 encoded string representing the random sequence.
+ */
 function encodeRandom(length: number): string {
   let str = '';
   for (let i = 0; i < length; i++) {
@@ -12,6 +22,13 @@ function encodeRandom(length: number): string {
   return str;
 }
 
+/**
+ * Encodes the current timestamp into a base32 string.
+ * 
+ * @param now The current timestamp in milliseconds since the Unix epoch.
+ * @param length The length of the timestamp string to generate.
+ * @return A base32 encoded string representing the timestamp.
+ */
 function encodeTime(now: number, length: number): string {
   let str = '';
   for (let i = length; i > 0; i--) {
@@ -22,6 +39,11 @@ function encodeTime(now: number, length: number): string {
   return str;
 }
 
+/**
+ * Generates a Universally Unique Lexicographically Sortable Identifier (ULID).
+ * 
+ * @return A string representing the ULID.
+ */
 export function generateULID(): string {
   const now = Date.now();
   const timestampPart = encodeTime(now, TIME_LEN);
